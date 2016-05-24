2c49c6f3d1a794b070d001f073e44bf1f2a297ef
Jake Eden <jakeeden1995@gmail.com>
2016-05-24 16:32:41 -0400

    Parallized a convergence condition. Fixed other pieces of the code.

--- pagerank_linked_mem_transfer.cu
+++ pagerank_linked_mem_transfer.cu
@@ -24,61 +24,134 @@ float abs_float(float in) {
 }
 
 __global__ void initializePagerankArray(float * pagerank_d, int n_vertices) {
-    int i = threadIdx.x;
+    int i = (blockIdx.x * blockDim.x) + threadIdx.x;
 
-    pagerank_d[i] = 1.0/(float)n_vertices;
+    if (i < n_vertices) {
+        pagerank_d[i] = 1.0/(float)n_vertices;
+    }
 }
 
-__global__ void setPagerankNextArray(float * pagerank_next_d) {
-    int i = threadIdx.x;
+__global__ void setPagerankNextArray(float * pagerank_next_d, int n_vertices) {
+    int i = (blockIdx.x * blockDim.x) + threadIdx.x;
 
-    pagerank_next_d[i] = 0.0;
+    if (i < n_vertices) {
+        pagerank_next_d[i] = 0.0;
+    }
 }
 
 
-__global__ void addToNextPagerankArray(float * pagerank_d, float * pagerank_next_d, int * n_successors_d, int * successors_d, int * successor_offset_d, float * dangling_value2) {
-    int i = threadIdx.x;
+__global__ void addToNextPagerankArray(float * pagerank_d, float * pagerank_next_d, int * n_successors_d, int * successors_d, int * successor_offset_d, float * dangling_value2, int n_vertices) {
+    int i = (blockIdx.x * blockDim.x) + threadIdx.x;
     int j;
+    int n_suc;
 
-    if(n_successors_d[i] > 0) {
-        for(j = 0; j < n_successors_d[i]; j++) {
-            atomicAdd(&(pagerank_next_d[successors_d[successor_offset_d[i]+j]]), 0.85*(pagerank_d[i])/n_successors_d[i]);
+    if (i < n_vertices) {
+        n_suc = n_successors_d[i];
+        if(n_suc > 0) {
+            for(j = 0; j < n_suc; j++) {
+                atomicAdd(&(pagerank_next_d[successors_d[successor_offset_d[i]+j]]), 0.85*(pagerank_d[i])/n_suc);
+            }
+        } else {
+            atomicAdd(dangling_value2, 0.85*pagerank_d[i]);
         }
-    }else {
-        atomicAdd(dangling_value2, 0.85*pagerank_d[i]);
     }
 }       
 
 
 __global__ void finalPagerankArrayForIteration(float * pagerank_next_d, int n_vertices, float dangling_value2) {
-    int i = threadIdx.x;
+    int i = (blockIdx.x * blockDim.x) + threadIdx.x;
 
-    pagerank_next_d[i] += (dangling_value2 + (1-0.85))/((float)n_vertices);
+
+    if(i < n_vertices) {
+        pagerank_next_d[i] += (dangling_value2 + (1-0.85))/((float)n_vertices);
+    }
 }
 
 
-__global__ void setPagerankArrayFromNext(float * pagerank_d, float * pagerank_next_d) {
-    int i = threadIdx.x;
+__global__ void setPagerankArrayFromNext(float * pagerank_d, float * pagerank_next_d, int n_vertices) {
+    int i = (blockIdx.x * blockDim.x) + threadIdx.x;
 
-    pagerank_d[i] = pagerank_next_d[i];
-    pagerank_next_d[i] = 0.0;
+    if(i < n_vertices) {
+        pagerank_d[i] = pagerank_next_d[i];
+        pagerank_next_d[i] = 0.0;
+    }
 }      
 
+__global__ void convergence(float * pagerank_d, float * pagerank_next_d, float * reduced_sums_d, int n_vertices) {
+// Each thread computes the diff for two vertexes (thus, half # of blocks needed for this function)
+// Because of this, we need to handle the case where only one block is needed
+    int i = (blockIdx.x * blockDim.x) + threadIdx.x;
+    int i_thr = threadIdx.x;
 
+    __shared__ float sums[1024];                       // blockDim.x == 1024
+    
+    float temp1, temp2;    
+    
+    if(i < 1024) {
+        reduced_sums_d[i] = 0;
+    }
+
+    if(i < n_vertices) {
+        temp1 = pagerank_next_d[i] - pagerank_d[i];
+        if(i + (1024 * gridDim.x) < n_vertices) {
+            temp2 = pagerank_next_d[i+ (1024 * gridDim.x)] - pagerank_d[i +(1024*gridDim.x)];
+        }else{
+            temp2 = 0;
+        }
+
+        if(temp1 < 0) {
+            temp1 = temp1 * (-1);
+        }
+        if(temp2 < 0) {
+            temp2 = temp2 * (-1);
+        }
+
+        sums[i_thr] = temp1 + temp2;
+    } else {
+        sums[i_thr] = 0;
+    }
+    __syncthreads();
+
+    int j, index, index2;
+    index = i_thr;
+
+    for(j = 0; j < 10; j++) {                    // 10 times as 2^10 = 1024 threads
+        if((index+1) % (2 * (1 << j)) == 0) {    // Note: 1 << j == 2^j
+            index2 = index - (1 << j);
+            sums[index] += sums[index2];
+        }
+        __syncthreads();
+    }
+
+    reduced_sums_d[blockIdx.x] = sums[1023];
+}
+
+__global__ void getConvergence(float * reduced_sums_d, float * diff) {
+    int j, index, index2;
+    index = threadIdx.x;
+    
+    for(j = 0; j < 10; j++) {                    // 10 times as 2^10 = 1024 threads
+        if((index+1) % (2 * (1 << j)) == 0) {    // Note: 1 << j == 2^j
+            index2 = index - (1 << j);
+            reduced_sums_d[index] += reduced_sums_d[index2];
+        }
+        __syncthreads();
+    }    
+
+    *diff = reduced_sums_d[1023]; 
+}
 
 int main(void) {
     // Error code to check return values for CUDA calls
     cudaFree(0);   // Set the cuda context here so that when we time, we're not including initial overhead
     cudaError_t err = cudaSuccess;
     
-
-/*************************************************************************/
     // Start CPU timer
     clock_t start = clock(), diff;
 
 /*************************************************************************/
     // build up the graph
-    int i;//,j;
+    int i;
     unsigned int n_vertices = 0;
     unsigned int n_edges = 0;
     unsigned int vertex_from = 0, vertex_to = 0;
@@ -108,7 +181,6 @@ int main(void) {
     }
     n_vertices++;
     
-
     // Allocate memory for vertices on host and device
     vertices = (vertex *) malloc(n_vertices * sizeof(vertex));
  
@@ -121,15 +193,7 @@ int main(void) {
     successor_offset_h = (int *) malloc(n_vertices * sizeof(*successor_offset_h));
     err = cudaMalloc((void **)&successor_offset_d, n_vertices*sizeof(int));
 
-
     // SET Initial Parameters  **********************************************************
-    /*  
-    unsigned int n_iterations = 25;
-    float alpha = 0.85;
-    float eps   = 0.000001;
-    */
-   
-
     if (!vertices) {
         fprintf(stderr,"Malloc failed for vertices.\n");
         exit(-1);
@@ -148,26 +212,16 @@ int main(void) {
     successors_h = (int *) malloc(n_edges * sizeof(*successors_h));
     err = cudaMalloc((void **)&successors_d, n_edges*sizeof(int));
 
-
     // allocate memory for successor pointers
     int offset = 0;                                         // offset into the successors_h array
 
     for (i=0; i<n_vertices; i++) {
-        //vertices[i].vertex_id = i;
         successor_offset_h[i] = offset;
     
         if (vertices[i].n_successors > 0) {
-           /* vertices[i].successors = (vertex **) malloc(vertices[i].n_successors * sizeof(vertex *));
-            
-            if (!vertices[i].successors) {
-                fprintf(stderr,"Malloc failed for successors of vertex %d.\n",i);
-                exit(-1);
-            }
-            memset((void *)vertices[i].successors, 0, (size_t)(vertices[i].n_successors*sizeof(vertex *)));*/
             offset += vertices[i].n_successors;
         }
         //else
-            // vertices[i].successors = NULL;
             // Flattened structure is basically a offset + 0 here
     }
 
@@ -178,19 +232,6 @@ int main(void) {
         // Flattened data structure code to fill successors_h
         successors_h[suc_index] = vertex_to;
         suc_index++;            
-        
-        /*        
-        for (i=0; i<vertices[vertex_from].n_successors; i++) {
-            if (vertices[vertex_from].successors[i] == NULL) {
-                vertices[vertex_from].successors[i] = &vertices[vertex_to];
-                break;
-            }
-            else if (i==vertices[vertex_from].n_successors-1) {
-                printf("Setting up the successor pointers of virtex %u failed",vertex_from);
-                return -1;
-            }
-        }
-        */
     }
 
     fclose(fp);
@@ -202,139 +243,127 @@ int main(void) {
     err = cudaMemcpy(successor_offset_d, successor_offset_h, n_vertices*sizeof(int), cudaMemcpyHostToDevice);
 
 /*************************************************************************/
+    int n_iterations = 30;
+    int iteration = 0;
+    int numOfBlocks = 1;                          // default example value for 1000 vertex graph
+    int threadsPerBlock = 1000;                   // default example value for 1000 vertex graph
+
+    if(n_vertices <= 1024) {
+        threadsPerBlock = n_vertices;
+        numOfBlocks = 1;
+    } else {
+        threadsPerBlock = 1024;
+        numOfBlocks = (n_vertices + 1023)/1024;   // The "+ 1023" ensures we round up
+    }
+
     // compute the pagerank on the GPU
     float dangling_value_h = 0;
     float dangling_value_h2 = 0;
-    float *dangling_value2;
-    
+    float *dangling_value2, *reduced_sums_d;
+    int n_blocks = (n_vertices + 2048 - 1)/2048;
+    if (n_blocks == 0){
+        n_blocks = 1;
+    }
+    float epsilon = 0.000001;
+    float * d_diff;
+    float h_diff = epsilon + 1;
+
+    err = cudaMalloc((void **)&d_diff, sizeof(float));
+    err = cudaMalloc((void **)&reduced_sums_d, 1024 * sizeof(float));     
     err = cudaMalloc((void **)&dangling_value2, sizeof(float));
     err = cudaMemcpy(dangling_value2, &dangling_value_h, sizeof(float), cudaMemcpyHostToDevice);
 
-    initializePagerankArray<<<1,46>>>(pagerank_d, n_vertices);
-    setPagerankNextArray<<<1, 46>>>(pagerank_next_d);
+    initializePagerankArray<<<numOfBlocks,threadsPerBlock>>>(pagerank_d, n_vertices);
+    setPagerankNextArray<<<numOfBlocks,threadsPerBlock>>>(pagerank_next_d, n_vertices);
     cudaDeviceSynchronize();
     
-    for(i = 0; i < 123; i++) {  //was 23
-      //  setPagerankNextArray<<<1,46>>>(pagerank_next_d);
-      //  cudaDeviceSynchronize();
-       
-        // set the dangling value to 0 
+    while(epsilon < h_diff && iteration < n_iterations) {  //was 23
+       // set the dangling value to 0 
         dangling_value_h = 0;
         err = cudaMemcpy(dangling_value2, &dangling_value_h, sizeof(float), cudaMemcpyHostToDevice);
        
- 
         // initial parallel pagerank_next computation
-        addToNextPagerankArray<<<1,46>>>(pagerank_d, pagerank_next_d, n_successors_d, successors_d, successor_offset_d, dangling_value2);
-        cudaDeviceSynchronize();
+        addToNextPagerankArray<<<numOfBlocks,threadsPerBlock>>>(pagerank_d, pagerank_next_d, n_successors_d, successors_d, successor_offset_d, dangling_value2, n_vertices);
 
         // get the dangling value
         err = cudaMemcpy(&dangling_value_h2, dangling_value2, sizeof(float), cudaMemcpyDeviceToHost); 
 
         // final parallel pagerank_next computation
-        finalPagerankArrayForIteration<<<1,46>>>(pagerank_next_d, n_vertices, dangling_value_h2);
+        finalPagerankArrayForIteration<<<numOfBlocks,threadsPerBlock>>>(pagerank_next_d, n_vertices, dangling_value_h2);
+
+        // Test for convergence
+        convergence<<<n_blocks,1024>>>(pagerank_d, pagerank_next_d, reduced_sums_d, n_vertices); 
+        getConvergence<<<1,1024>>>(reduced_sums_d, d_diff);
+        
+        // Get difference to compare to epsilon
+        cudaMemcpy(&h_diff, d_diff, sizeof(float), cudaMemcpyDeviceToHost);
+        
+        // Make pagerank_d[i] = pagerank_next_d[i]
+        setPagerankArrayFromNext<<<numOfBlocks,threadsPerBlock>>>(pagerank_d, pagerank_next_d, n_vertices);
         cudaDeviceSynchronize();
 
-        // Make pagerank_d[i] = pagerank_next_d[i]
-        setPagerankArrayFromNext<<<1,46>>>(pagerank_d, pagerank_next_d);
-        cudaDeviceSynchronize(); 
+        iteration++;
     }
 
 
-/*****************************************************************************************/ 
-  // Compute pagerank on host using old method for comparison purposes
-  /*  unsigned int i_iteration;
-
-    float value, diff;
-    float pr_dangling_factor = alpha / (float)n_vertices;   // pagerank to redistribute from dangling nodes
-    float pr_dangling;
-    float pr_random_factor = (1-alpha) / (float)n_vertices; // random portion of the pagerank
-    float pr_random;
-    float pr_sum, pr_sum_inv, pr_sum_dangling;
-    float temp;
-
-    // initialization of values before pagerank loop
-    for (i=0;i<n_vertices;i++) {
-        vertices[i].pagerank = 1 / (float)n_vertices;
-        vertices[i].pagerank_next =  0;
-    }
-
-    pr_sum = 0;
-    pr_sum_dangling = 0;
-    for (i=0; i<n_vertices; i++) {
-        pr_sum += vertices[i].pagerank;
-        if (!vertices[i].n_successors)
-            pr_sum_dangling += vertices[i].pagerank;
-    }
-
-    i_iteration = 0;
-    diff = eps+1;
-*/
     //****** transfer pageranks from device to host memory ************************************************ 
     err = cudaMemcpy(pagerank_h, pagerank_d, n_vertices*sizeof(float), cudaMemcpyDeviceToHost);
 
     // Find CPU elapsed time
     diff = clock() - start;
-    printf("diff: %Lf\n", (long double)diff); 
+    //printf("diff: %Lf\n", (long double)diff); 
+    
     // Print time taken
     int millisec = diff * 1000 / CLOCKS_PER_SEC;
     printf("Time taken: %d milliseconds\n", millisec%1000);
-    
+ 
     // Print pageranks
     for(i = 0; i < n_vertices; i++) {
         printf("i: %d, pr: %.6f\n",i, pagerank_h[i]);
     }    
 
-    //********************************************************************************************* 
-  /*  while ( (diff > eps) && (i_iteration < n_iterations) ) {
+    printf("Time taken: %d seconds, %d milliseconds\n",millisec/1000, millisec%1000);
+    printf("iter: %d\n", iteration);
 
-        for (i=0;i<n_vertices;i++) {
-            if (vertices[i].n_successors)
-                value = (alpha/vertices[i].n_successors)*vertices[i].pagerank;  //value = vote value after splitting equally
-            else
-                value = 0;
-            //printf("vertex %d: value = %.6f \n",i,value);
-            for (j=0;j<vertices[i].n_successors;j++) {               // pagerank_next = sum of votes linking to it
-                vertices[i].successors[j]->pagerank_next += value;
-            }
-        }
-    
-        // for normalization
-        pr_sum_inv = 1/pr_sum;
+// Tests: ***************************
+/*    int n_vert = 1000;    
 
-        // alpha
-        pr_dangling = pr_dangling_factor * pr_sum_dangling;
-        pr_random = pr_random_factor * pr_sum;
+    n_blocks = (n_vert + 2048 - 1)/2048;
+    if(n_blocks == 0){
+        n_blocks = 1;
+    } 
 
-        pr_sum = 0;
-        pr_sum_dangling = 0;
+    float * h_A = (float *) malloc(n_vert * sizeof(float));
+    float * h_B = (float *) malloc(n_vert * sizeof(float));
+    float * d_A = NULL;
+    float * d_B = NULL;
+    float * d_C = NULL;
+    err = cudaMalloc((void **)&d_A, n_vert * sizeof(float));
+    err = cudaMalloc((void **)&d_B, n_vert * sizeof(float)); 
+    err = cudaMalloc((void **)&d_C, 1024 * sizeof(float));     
 
-        diff = 0;
-        for (i=0;i<n_vertices;i++) {
-            // update pagerank
-            temp = vertices[i].pagerank;
-            vertices[i].pagerank = vertices[i].pagerank_next*pr_sum_inv + pr_dangling + pr_random;
-            vertices[i].pagerank_next = 0;
-
-            // for normalization in next cycle
-            pr_sum += vertices[i].pagerank;
-            if (!vertices[i].n_successors)
-                pr_sum_dangling += vertices[i].pagerank;
-
-            // convergence
-            diff += abs_float(temp - vertices[i].pagerank);
-        }
-        printf("Iteration %u:\t diff = %.12f\n", i_iteration, diff);
-
-        i_iteration++;
+    for(i = 0; i < n_vert; i++) {
+        h_A[i] = 1;
+        h_B[i] = 1.0009;
     }
-*/
-/*************************************************************************/
-    // print the pageranks from this host computation
-  /*  for (i=0;i<n_vertices;i++) {
-        printf("Vertex %u:\tpagerank = %.6f\n", i, vertices[i].pagerank);
-    }
-  */
-/*************************************************************************/
+    err = cudaMemcpy(d_A, h_A, n_vert *sizeof(float), cudaMemcpyHostToDevice);
+    err = cudaMemcpy(d_B, h_B, n_vert*sizeof(float), cudaMemcpyHostToDevice);
+
+    convergence<<<n_blocks,1024>>>(d_A, d_B, d_C, n_vert); 
+    cudaDeviceSynchronize();
+
+  //  float * d_diff;
+   // float h_diff;
+    err = cudaMalloc((void **)&d_diff, sizeof(float));
+
+     getConvergence<<<1,1024>>>(d_C, d_diff);
+    cudaDeviceSynchronize();
+
+    cudaMemcpy(&h_diff, d_diff, sizeof(float), cudaMemcpyDeviceToHost);
+    printf("this: %.3f\n", h_diff);
+     */
+
+// End Tests ************************
 
     // Free device global memory
     err = cudaFree(pagerank_d);
